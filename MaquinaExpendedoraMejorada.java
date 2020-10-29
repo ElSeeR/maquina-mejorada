9a3bd4367cabd2e5e4e772cb8c2af491fb34af53
ElSeeR <sergio9garcia9@gmail.com>
2020-10-29 12:47:22 +0100

    0745.02 Añadido metodo vaciar dinero

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -85,4 +85,10 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         return cantidadDeDineroADevolver;
     } 
+    
+    public int vaciarDineroDeLaMaquina() {
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        return totalDineroAcumulado;
+    }
 }
